023b18f71eb2d9461dd40f382d51cb7214474e25
22fi065 <166772094+22fi065@users.noreply.github.com>
2024-06-14 12:41:02 +0900

    ﾆｰﾊｵﾆｬﾝ

--- main.py
+++ main.py
@@ -7,7 +7,23 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": from fastapi.responses import HTMLResponse #インポート
+
+### コードいろいろ... ###
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
